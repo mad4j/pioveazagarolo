5631635710a5fe27f3884c612b6559e8eb3ed18e
Daniele Olmisani <daniele.olmisani@gmail.com>
2025-05-10 08:11:58 +0200

    added watermark

--- service-worker.js
+++ service-worker.js
@@ -4,6 +4,7 @@ const urlsToCache = [
   "./index.html",
   "./data.json",
   "./favicon.png",
+  "watermark-512x512.png",
   "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css",
   "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js",
   "https://cdn.jsdelivr.net/npm/chart.js"
