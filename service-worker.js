500e6020738cfd5c7554e417dc31d14fe22284d2
Daniele Olmisani <daniele.olmisani@gmail.com>
2025-05-17 21:42:17 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -8,9 +8,9 @@ const ASSETS_TO_CACHE = [
   '/favicon.png',
   '/css/weather-icons.min.css',
   '/manifest.json',
-  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css',
-  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js',
-  'https://cdn.jsdelivr.net/npm/chart.js'
+ // 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css',
+ // 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js',
+ // 'https://cdn.jsdelivr.net/npm/chart.js'
 ];
 
 // Installazione del Service Worker
