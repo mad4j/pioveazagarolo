de7f7afac7e56ac1934a0742567c331cf47877ce
Daniele Olmisani <daniele.olmisani@gmail.com>
2025-10-11 10:10:57 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Incrementare CACHE_NAME per forzare l'installazione di un nuovo Service Worker
-const CACHE_NAME = "piove-a-zagarolo-cache-v34";
+const CACHE_NAME = "piove-a-zagarolo-cache-v35";
 const urlsToCache = [
   "./",
   "./index.html",
